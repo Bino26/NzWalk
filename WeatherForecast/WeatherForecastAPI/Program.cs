364082023e50123332a1edad730a834661fcb4b7
Bino26 <hbino1999@gmail.com>
2024-03-11 11:26:45 +0000

    Add domain models

---
+++ WeatherForecast/WeatherForecastAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForecastAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

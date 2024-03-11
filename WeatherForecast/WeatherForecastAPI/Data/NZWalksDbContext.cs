c3d068940332bfee3a3e74928300b236bdfacdde
Bino26 <hbino1999@gmail.com>
2024-03-11 13:56:18 +0000

    Create DbContext.cs

---
+++ WeatherForecast/WeatherForecastAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecastAPI.Models.Domain;
+
+namespace WeatherForecastAPI.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Difficulty> Difficulties  { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+         
+
+
+    }
+}

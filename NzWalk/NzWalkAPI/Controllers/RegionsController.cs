10f2d3d6c4a8592016911a00a2274bc8a84ef887
Bino26 <hbino1999@gmail.com>
2024-03-18 17:20:59 +0000

    Update RegionsController.cs and Migrations

---
+++ NzWalk/NzWalkAPI/Controllers/RegionsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NzWalkAPI.Data;
+using NzWalkAPI.Models.Domain;
+
+namespace NzWalkAPI.Controllers
+{
+    [Route("api/regions")]
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+
+        //Get All Regions
+    {
+        private readonly NzWalkDbContext dbContext;
+
+        public RegionsController(NzWalkDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();   
+            
+            return Ok(regions);
+
+
+        }
+    }
+}

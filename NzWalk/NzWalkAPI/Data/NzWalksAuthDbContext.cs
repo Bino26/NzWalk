17bef5f3e6bc97ad5f8f4516ac9046215e480dee
Bino26 <hbino1999@gmail.com>
2024-04-06 23:58:29 +0000

    Seeding Roles

--- NzWalk/NzWalkAPI/Data/NzWalksAuthDbContext.cs
+++ NzWalk/NzWalkAPI/Data/NzWalksAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NzWalkAPI.Data
@@ -8,6 +9,37 @@ namespace NzWalkAPI.Data
         public NzWalksAuthDbContext(DbContextOptions<NzWalksAuthDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "715436ed-afc0-4b9d-b8f1-859aa6f73edb";
+            var writerRoleId = "e54e233a-069b-4827-a33f-2e822e668054";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId ,
+                   ConcurrencyStamp = readerRoleId,
+                   Name = "Reader",
+                   NormalizedName = "Reader".ToUpper()
+
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId ,
+                   ConcurrencyStamp = writerRoleId,
+                   Name = "Writer",
+                   NormalizedName = "Writer".ToUpper()
+
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+
+
 
     }
 }
